66a4893f862678b90116ad0708438297a0ae8bb5
Jen Barbettini <jen@hint.com>
2018-08-17 13:49:36 -0700

    Implement AWS Lambda function to shuffle meeting order

---
+++ index.js
@@ -0,0 +1,35 @@
+const _ = require('lodash');
+const axios = require('axios');
+const moment = require('moment');
+
+async function intro() {
+  let dayOfWeek = moment().format("dddd");
+  let message = `Happy ${dayOfWeek}! Here is the standup order for today:`;
+  return message;
+}
+
+async function teamList() {
+  let team = ["Adrian", "Agus", "Gabo", "Graham", "Greg", "Jen", "Joaco", "Maicol", "Mati", "Miguel", "Ricardo", "Santi", "Saurabh"];
+  let sortedTeam = _.shuffle(team).join('\n');
+  return sortedTeam;
+}
+
+async function randomColor() {
+  return '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+}
+
+async function postToSlack() {
+  await axios.post('https://hooks.slack.com/services/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', {
+    text: await intro(),
+    attachments: [
+      {
+        "text": await teamList(),
+        "color": await randomColor(),
+      }
+    ]
+  });
+}
+
+exports.handler = async(event, context) => {
+  await postToSlack();
+}
